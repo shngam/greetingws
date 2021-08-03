4c7d8eedfa8895e648f788c429e544db080cca97
BRSHNGAM <BRSHNGAM@N5180.slake.sssb.com>
2021-08-03 16:01:06 +0800

    First Commit

---
+++ greetingws/src/main/java/com/example/greetingws/GreetingComponent.java
@@ -0,0 +1,10 @@
+package com.example.greetingws;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+	public String getMessage() {
+		return "Hello from GreetingComponenet";
+	}
+}
